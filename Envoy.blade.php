b30f0b568f913b5eef28e5e6241a507bfdb24db2
Geoff St. Pierre <serundeputy@gmail.com>
2018-02-18 11:26:29 -0500

    Adding envoy deploy script.

---
+++ Envoy.blade.php
@@ -0,0 +1,16 @@
+@servers(['web' => ['gff@192.155.88.91']])
+
+@task('ll', ['on' => 'web'])
+  cd /var/www/serundeputy
+  ls -alh
+@endtask
+
+@task('deploy', ['on' => 'web'])
+  cd /var/www/serundeputy/www
+  @if ($branch)
+    git pull origin {{ $branch }}
+  @endif
+  drush updb -y
+  drush bcim -y
+  drush cc all
+@endtask
