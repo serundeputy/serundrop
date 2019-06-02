fa3e1e9eb716c42a1dad696ed0c9c539d481b697
Geoff St. Pierre <serundeputy@gmail.com>
2019-06-02 09:46:47 -0400

    Adding tugboat settings file.

---
+++ .tugboat/settings.local.php
@@ -0,0 +1,21 @@
+<?php
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'tugboat',
+      'username' => 'tugboat',
+      'password' => 'tugboat',
+      'host' => 'mysql',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+$config_directories['active'] = '../config/active';
+$config_directories['staging'] = '../config/staging';
+
+$settings['hash_salt'] = 'EnCIM4QLvsSChnHd5vkVlVgZq9EF2UMelLuKYng4vvg';
